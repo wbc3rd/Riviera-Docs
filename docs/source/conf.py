301520ea2d4eac7ef68602b6e4ee05d00bb477ae
davidkott <124292136+davidkott@users.noreply.github.com>
2025-05-07 23:32:08 -0700

    updating list of headers as a test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Riviera'
+copyright = '2025, Kott'
+author = 'Kott and C Prizzy'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
